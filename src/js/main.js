76a4ef2ec40682b9d18fd60da520fa0b93fbaf83
artur <akh@kodix.ru>
2017-10-06 17:03:32 +0300

    Index desktop version

--- src/js/main.js
+++ src/js/main.js
@@ -37,26 +37,124 @@ $(document).ready(function () {
     $('#video').attr('src', videoSrc);
   });
 
-  ymaps.ready(init);
-  var myMap,
-    myPlacemark;
-
-  function init() {
-    console.log('hey');
-    myMap = new ymaps.Map("map", {
-      center: [56.00816307, 37.85472750],
-      zoom: 16,
-      controls: ['zoomControl', 'geolocationControl']
+  if($('.js_slick-features') && window.innerWidth > 1024) {
+    $('.js_slick-features').slick({
+      slidesToShow: 4,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true,
+            arrows: true
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: true
+          }
+        }
+      ]
     });
+  }
 
-    myPlacemark = new ymaps.Placemark([56.00816307, 37.85472750], {
-      hintContent: 'Диадент!',
-      balloonContent: 'Московский проспект, 27'
-    }, {
-      preset: 'islands#redDotIcon'
+  if ($('.js_slick-doctors')) {
+    $('.js_slick-doctors').slick({
+      dots: true,
+      arrows: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true,
+            arrows: true
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: true
+          }
+        }
+      ]
     });
+  }
 
-    myMap.geoObjects.add(myPlacemark);
-    myMap.behaviors.disable('scrollZoom');
+  if ($('.js_slick-certificates')) {
+    $('.js_slick-certificates').each(function() {
+      $(this).slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        variableWidth: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }]
+      });
+    });
+  }
+
+  if (ymaps) {
+    ymaps.ready(init);
+    var myMap,
+      myPlacemark;
+
+    function init() {
+      myMap = new ymaps.Map("map", {
+        center: [56.00816307, 37.85472750],
+        zoom: 16,
+        controls: ['zoomControl', 'geolocationControl']
+      });
+
+      myPlacemark = new ymaps.Placemark([56.00816307, 37.85472750], {
+        hintContent: 'Диадент!',
+        balloonContent: 'Московский проспект, 27'
+      }, {
+        preset: 'islands#redDotIcon'
+      });
+
+      myMap.geoObjects.add(myPlacemark);
+      myMap.behaviors.disable('scrollZoom');
+    }
   }
 });
